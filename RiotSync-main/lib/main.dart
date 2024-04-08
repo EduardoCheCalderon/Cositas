e64d78a6abb7303403343ef60ab09ca5e75f5208
EduardoCheCalderon <127123128+EduardoCheCalderon@users.noreply.github.com>
2024-04-08 16:37:27 -0600

    database

--- RiotSync-main/lib/main.dart
+++ RiotSync-main/lib/main.dart
@@ -1,11 +1,18 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:riot_sync/router/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:riot_sync/services/firebase_service.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
-  runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseFirestore.instance.settings =
+      const Settings(persistenceEnabled: true);
+  (const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,6 +24,22 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
+      body: FutureBuilder(
+          future: getRegistro(),
+          builder: ((context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data?.length,
+                itemBuilder: (context, index) {
+                  return Text(snapshot.data?[index]['nombre']);
+                },
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          })),
       debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.initialRoute,
       routes: AppRoutes.routes,
